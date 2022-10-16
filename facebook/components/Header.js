569542b07e832c6b5cc7fc516ae3b351247ef144
ValentinoMartz <valentinomartinez55@gmail.com>
2022-10-16 11:13:43 -0300

    header template

--- facebook/components/Header.js
+++ facebook/components/Header.js
@@ -3,7 +3,6 @@ import React from "react";
 import facebook from "../assets/facebook1.png";
 import user from "../assets/guy7.jpg";
 import nouser from "../assets/nouser.png";
-
 import { MdHome } from "react-icons/md";
 import { FiPlayCircle, FiFlag, FiMessageCircle } from "react-icons/fi";
 import { BsCart3 } from "react-icons/bs";
@@ -13,17 +12,17 @@ import { AiOutlineMessage } from "react-icons/ai";
 
 const Header = () => {
   return (
-    <div className="p-4 flex items-center justify-between">
+    <div className="p-4 flex items-center justify-between border-b lg:px-8">
       {/* LeftSide */}
-      <div className="flex">
+      <div className="flex items-center  mr-4">
         <div className="w-10 h-10">
           <Image src={facebook} />
         </div>
-        <div className="ml-2 items-center">
+        <div className="ml-2">
           <input
             type="text"
             placeholder="Search Facebook"
-            className="outline-0 bg-[#f2f3f7] p-2 rounder-full pl-4"
+            className="outline-0 bg-[#f2f3f7] p-2 rounder-full pl-4 hidden sm:block"
           />
         </div>
       </div>
@@ -35,8 +34,18 @@ const Header = () => {
         <BsCart3 className="w-7 h-7" />
         <GrGroup className="w-7 h-7" />
       </div>
-
       {/* RightSide */}
+      <div className="flex space-x-6 items-center ml-2">
+        <div className="md:flex space-x-6 hidden">
+          <GrAppsRounded className="w-7 h-7" />
+          <FaBell className="w-7 h-7" />
+          <AiOutlineMessage className="w-7 h-7" />
+        </div>
+
+        <div className="w-10 h-10">
+          <Image src={user} className="rounded-full" />
+        </div>
+      </div>
     </div>
   );
 };
